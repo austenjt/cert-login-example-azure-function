5c4b186e0778f0893d5649f528c6fa417e60f73a
Jon Austen <djangofan@gmail.com>
2024-01-08 16:38:38 -0800

    Minor edit.

--- src/main/java/org/example/functions/HttpTriggerJava.java
+++ src/main/java/org/example/functions/HttpTriggerJava.java
@@ -7,18 +7,19 @@ import com.microsoft.azure.functions.*;
 public class HttpTriggerJava {
 
 
-    @FunctionName("HttpTriggerJava")
-    public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+    @FunctionName("Hello")
+    public HttpResponseMessage runHello(
+            @HttpTrigger(name = "hello", methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
         String query = request.getQueryParameters().get("name");
         String name = request.getBody().orElse(query);
 
         if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body")
+                .build();
         } else {
             return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
         }
